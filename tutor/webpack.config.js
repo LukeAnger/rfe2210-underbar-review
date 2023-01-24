8b92941a344f2dee49de7ec443b3fc5117bae7a8
root <root@DESKTOP-0JF4NCL.localdomain>
2023-01-24 11:33:52 -0500

    tutor backup

---
+++ tutor/webpack.config.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  mode: 'development',
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/client/dist'),
+    },
+    compress: true,
+    port: 3140,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ],
+            plugins: [
+              ["@babel/plugin-transform-runtime",
+                {
+                  "regenerator": true
+                }
+              ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }
+    ],
+  }
+};
